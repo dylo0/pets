cc5cf93d93f5613df53f3b15fa67a22294976ebb
Tomasz Dyl <tomekdyl@gmail.com>
2014-05-22 16:57:50 +0200

    git initialization

---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(document).ready(function() {
+    $("#owl-example").owlCarousel({
+        autoPlay: true,
+        stopOnHover: true
+    });
+
+    $('.flip-container').click(function () {
+    	$(this).toggleClass('flip');
+    })
+});
